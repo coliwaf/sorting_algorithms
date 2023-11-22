645e36c2488aef226aa87530a5f7b0a298311924
Collins Sande <coliwafula@gmail.com>
2023-11-22 04:44:12 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,21 +10,20 @@
 void selection_sort(int *array, size_t size)
 {
 	int temp = 0;
-	size_t i = 0, j, k = 0;
+	size_t i = 0, j, k;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (; i < size - 1; i++)
 	{
-		k = i;
-		for (j = i + 1; j < size; j++)
+		for (j = size - 1, k = i + 1; j > i; j--)
 		{
 			if (array[j] < array[k])
 				k = j;
 		}
 
-		if (k != 1)
+		if (array[i] > array[k])
 		{
 			temp = array[i];
 			array[i] = array[k];
